21317224117c9bd43f90b48c01844e28d3811053
Ahmed Ur Rehman <160028006+Ahmedkhang@users.noreply.github.com>
2025-03-18 20:01:54 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default  async function Home() {
 
   return (
 
-    <div className='h-full bg-white w-full'>
+    <div className='h-full bg-gray-800 w-full'>
       <h1 className='w-full text-center  pt-5 text-2xl md:text-3xl lg:text-4xl font-bold text-blue-400'>Most Recent Blogs</h1>
       <div className='grid grid-cols- md:grid-cols-2 lg:grid-cols-3  pt-10'>
      {posts.map((posts:Post) => (
